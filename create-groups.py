13acb1a9f549d57b56fcb2c17663e6d0d1c36e93
anubis-code <paul.smith@kubekloud.co.uk>
2023-05-02 19:35:53 +0100

    files

---
+++ create-groups.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import urllib3
+
+with open('./group.json', 'r') as f:
+    jfm_dict = json.load(f)
+
+for jfm in jfm_dict:
+    print(jfm['path'])
+    gitlab_url = "https://testserver/gitlab/api/v4/groups"
+    headers = {'Content-type': 'application/json', 'PRIVATE-TOKEN': '<TOKEN>'}
+    data = {'name': jfm['path'], 'path': jfm['path'], 'visibility': jfm['visibility']}
+    urllib3.disable_warnings()
+    r = requests.post(gitlab_url, data=json.dumps(data), headers=headers, verify=False)
---
+++ create-repo.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import urllib3
+
+with open('./repo.json', 'r') as f:
+    jfm_dict = json.load(f)
+
+for jfm in jfm_dict:
+    print(jfm['name'])
+    gitlab_url = "http://gitlab.example.com:1337/api/v4/projects"
+    headers = {'Content-type': 'application/json', 'PRIVATE-TOKEN': '<TOKEN>'}
+    data = {'name': jfm['name'], 'namespace_id': jfm['namespace_id'], 'visibility': jfm['visibility']}
+    urllib3.disable_warnings()
+    r = requests.post(gitlab_url, data=json.dumps(data), headers=headers, verify=False)
